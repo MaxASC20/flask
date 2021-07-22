edeecf6ba7e4bff58377b431057e08b887b0bc09
maxASC20 <m.j.bardales9@gmail.com>
2021-07-22 18:01:27 +0000

    This should work plx

--- app.py
+++ app.py
@@ -7,10 +7,21 @@ from flask import Flask
 
 # -- Initialization section --
 app = Flask(__name__)
+money = {
+  "NY": 2650,
+  "LA": 1995,
+  "Houston": 1229
+}
+year = float(input('Enter your yearly income '))
+months = float(input('Enter the number of months'))
+var1 = float(year / months)
+print(f'Your monthly income is {var1} dollars')
 
+x = input('Enter the place you want to live between NY, LA, or Houston:')
+print(f'The average cost of a one bedroom apartment is {money[x]} dollars.')
 
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return "is this working??"
